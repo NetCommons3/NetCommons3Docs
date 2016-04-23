8eacd80321fce6b9a75855a89a9b14a7be3cc4f0
s-nakajima <nakajimashouhei@gmail.com>
2016-04-24 06:12:19 +0900

    自動でPHPDocを作成するように修正 https://github.com/NetCommons3/NetCommons3/issues/121

--- tools/build/app/travis/post.sh
+++ tools/build/app/travis/post.sh
@@ -3,5 +3,5 @@
 cd $NETCOMMONS_BUILD_DIR
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   git pull
-  [ $GH_TOKEN ] && git push --quiet https://$GH_TOKEN@github.com/$NETCOMMONS3DOCS.git
+  [ $GH_TOKEN ] && git push --quiet https://$GH_TOKEN@github.com/$NETCOMMONS3DOCS.git gh-pages
 fi
