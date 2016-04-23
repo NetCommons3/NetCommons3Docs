8b5fcfee5fc3ae755cf28f53c43abe78080cbf95
Shohei Nakajima <nakajimashouhei@gmail.com>
2016-04-23 15:01:13 +0900

    Update main2.sh

--- tools/build/app/travis/main2.sh
+++ tools/build/app/travis/main2.sh
@@ -34,7 +34,9 @@ do
     rm -r phpdoc/$plugin
   fi
   echo "phpdoc ${APP_ROOT}/Plugin/$plugin"
-  phpdoc run -d "$PHPDOC_OPTIONS,${APP_ROOT}/Plugin/$plugin" -t phpdoc/$plugin -i "$IGNORE_PLUGINS,${APP_ROOT}/Plugin/$plugin/Test/*" --force --ansi --log $LOG2 > $LOG
+  phpdoc run -d "$PHPDOC_OPTIONS,${APP_ROOT}/Plugin/$plugin" -t phpdoc/$plugin -i "$IGNORE_PLUGINS,${APP_ROOT}/Plugin/$plugin/Test/*" --cache-folder phpdoc/$plugin/cache --force --ansi --log $LOG2 > $LOG
+
+  rm -r phpdoc/$plugin/cache
 
   if [ "$TRAVIS_BRANCH" == "master" ]; then
     git add -A
